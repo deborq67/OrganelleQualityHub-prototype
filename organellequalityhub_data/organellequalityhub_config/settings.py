0070a985186a11484405848f21d8ac82432e1534
deborq67 <debohorquez@mail.fhsu.edu>
2026-08-19 23:12:57 -0500

    Added log for testing purposes.

--- organellequalityhub_data/organellequalityhub_config/settings.py
+++ organellequalityhub_data/organellequalityhub_config/settings.py
@@ -118,6 +118,23 @@ DATABASES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
 DATABASE_ROUTERS = ["organellequalityhub_config.routers.SupabaseRouter"]
 
 
